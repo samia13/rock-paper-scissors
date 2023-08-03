589a3ee1acbaba434db6d65706b9e88ad200c7af
Mahi samia <mahisamia13@gmail.com>
2023-08-03 23:14:23 +0100

    removing extra logs

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
-//  define the list of valid moves
+// Define the list of valid moves
 const validMoves = ["rock", "paper", "scissors"];
-let maxRounds = 5;
+const maxRounds = 5;
 
 function getComputerSelection() {
-  let index = Math.floor(Math.random() * validMoves.length);
+  const index = Math.floor(Math.random() * validMoves.length);
   return validMoves[index];
 }
 
@@ -22,91 +22,81 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function getValidWordFromUser(round) {
-  // no playing untill the user enters a valid move nihahahaha
-  while (true) {
-    let userInput = prompt(`Round ${round}: Enter your selection`);
+  let userInput;
+  do {
+    userInput = prompt(
+      `Round ${round} - Enter your selection (rock, paper, scissors):`
+    );
 
-    // if user typed quit or clicked on cancel
-    if (userInput == "quit" || userInput == null) {
+    if (userInput === "quit" || userInput === null) {
       return "quit";
     }
 
     userInput = userInput.trim().toLowerCase();
 
-    // if user entered nothingand just pressed enter, or provided unvalid moves
-    if (!userInput || !validMoves.includes(userInput)) {
-      console.log(
-        `Oops you made a mistake, ${
-          !userInput
-            ? "please type something, we can't accept void"
-            : "you typed " + userInput
-        },it is an invalid input`
-      );
-      alert(
-        "A little focus here (-___-)! Please enter only valid moves (rock, paper, scissors)."
-      );
-    } else {
-      return userInput;
+    if (!validMoves.includes(userInput)) {
+      console.log(`Invalid input: "${userInput}". Please enter a valid move.`);
     }
-  }
+  } while (!validMoves.includes(userInput));
+
+  return userInput;
 }
 
 function game() {
-  console.log(
-    "***************************************************************************"
-  );
-  console.log(
-    "Welcome to rock, paper, scissors game! \nwe count on you to save us From mister computer villain, Do us proud Hero \nList of valid Moves: rock, paper, scissors, please make sure to select one of these\n Keep in mind that You have 5 rounds to win the game, Good Luck \nTo quit, press the prompt cancel button, or type quit "
-  );
-  console.log(
-    "***************************************************************************"
-  );
+  console.clear();
+  console.log(`
+***************************************************************************
+Welcome to the rock, paper, scissors game!
+You have 5 rounds to win the game. Good Luck!
+To quit, press the prompt cancel button or type "quit".
+***************************************************************************
+`);
+
   let userScore = 0;
-  let computerScroe = 0;
-  for (let i = 1; i <= maxRounds; i++) {
-    const compSelect = getComputerSelection();
+  let computerScore = 0;
 
-    let validUserInput = getValidWordFromUser(i);
+  for (let round = 1; round <= maxRounds; round++) {
+    const computerSelect = getComputerSelection();
+    const validUserInput = getValidWordFromUser(round);
 
-    // if user types quit, exit the game
-    if (validUserInput == "quit") {
-      console.log(`You chose to quit, Next Hero please!`);
+    if (validUserInput === "quit") {
+      console.log(`>> You chose to quit. Next Hero please!`);
       return;
     }
 
-    // else play a Round
-    let result = playRound(validUserInput, compSelect);
+    const result = playRound(validUserInput, computerSelect);
 
     switch (result) {
       case -1:
-        computerScroe++;
-        console.log(`You Lose, ${compSelect} beats ${validUserInput}`);
+        computerScore++;
+        console.log(`>> You Lose, ${computerSelect} beats ${validUserInput}`);
         break;
       case 0:
-        console.log("it's a tie");
+        console.log(">> It's a tie");
         break;
       case 1:
         userScore++;
         console.log(
-          `Congratulations! You win! ${validUserInput} beats ${compSelect}`
+          `>> Congratulations! You win! ${validUserInput} beats ${computerSelect}`
         );
         break;
     }
   }
-  if (computerScroe < userScore)
+
+  if (computerScore < userScore) {
     console.log(
-      `You scored: ${userScore}, while villain AI scored ${computerScroe} \nThe world is saved, let's have some free food`
+      `>> You scored: ${userScore}, while the villain AI scored ${computerScore}. The world is saved, let's have some free food`
     );
-  else
+  } else if (computerScore === userScore) {
     console.log(
-      `You scored: ${userScore}, while villain AI scored ${computerScroe} \nThe world is doomed, Villain computer will take over now, Rest in peace yall`
+      `>> You scored: ${userScore}, same as the villain AI scored ${computerScore}. Let's give it another try`
     );
+  } else {
+    console.log(
+      `>> You scored: ${userScore}, while the villain AI scored ${computerScore}. The world is doomed, Villain computer will take over now, Rest in peace yall`
+    );
+  }
 }
 
 let startGameBtn = document.querySelector(".startGame");
-
-startGameBtn.addEventListener("click", () => {
-  console.clear();
-  game();
-});
-// game();
+startGameBtn.addEventListener("click", game);
